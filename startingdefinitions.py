ae4abaf4b28378cfca7974b66bcac23e62c61fc1
AIK <92125439+warhammer-ste@users.noreply.github.com>
2025-12-08 08:16:43 -0600

    Create startingdefinitions.py
    forgot to add startingdef from pastebin
    it's in main rather than a folder for my quick access

---
+++ startingdefinitions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.media.ev3dev import Font
+
+ev3 = EV3Brick()
+left_thing_that_runs = Motor(Port.C)
+right_motor = Motor(Port.B)
+sacrifice = Motor(Port.A) # Third evil motor that does usually nothing
+creek4win = DriveBase(left_thing_that_runs, right_motor, wheel_diameter=57.15, axle_track=88.9)
+
+# This file entirely exists because the presets stay the same basically all the time.
+# It has most of my preferred imports and it contains definitions. Please feel free to remove and edit things as you need.
