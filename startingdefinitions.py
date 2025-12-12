0b1e0735cfa0f3062229b80cc757aac5f616326c
AIK <92125439+warhammer-ste@users.noreply.github.com>
2025-12-12 08:35:01 -0600

    NXT import line
    Added a import line for NXT devices that don't have EV3 counterparts.

--- startingdefinitions.py
+++ startingdefinitions.py
@@ -2,6 +2,7 @@
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.nxtdevices import (TemperatureSensor, SoundSensor, EnergyMeter, VernierAdapter)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
