79452ab3f12c169a550403fa9c6b2c1f85f437c3
AIK <92125439+warhammer-ste@users.noreply.github.com>
2025-12-15 08:32:33 -0600

    Update main.py
    Attempting to program something that allows you to change basetemp without having to change internal code.

--- when_the_aliens_arrive/main.py
+++ when_the_aliens_arrive/main.py
@@ -8,32 +8,61 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-
 ev3 = EV3Brick()
 left_thing_that_runs = Motor(Port.C)
 right_motor = Motor(Port.B)
 sacrifice = Motor(Port.A) # Third evil motor that does usually nothing
 creek4win = DriveBase(left_thing_that_runs, right_motor, wheel_diameter=57.15, axle_track=88.9)
 theProbe = TemperatureSensor(Port.S1)
-tempies = theProbe.temperature()
-def printtempies():
-    ev3.screen.print(theProbe.temperature())
+roomtemp = 20
+mewanttempies = 0
+# def printtempies():
+#     print("tempies", theProbe.temperature())
+#     ev3.screen.print(theProbe.temperature())
+#     wait(200)
+#     ev3.screen.clear()
+def eviltempies():
+    evilfuckingtempies = round(theProbe.temperature()) # Default value for referencing temp
+    eviltempies = evilfuckingtempies - roomtemp # Value to reference temp changed by roomtemp
+    print("eviltempies", evilfuckingtempies)
+    ev3.screen.print(evilfuckingtempies)
     wait(200)
     ev3.screen.clear()
 while True:
-    printtempies()
+    ev3.screen.print("Room temp = Avg") # Alternatively; "Set room temp the same as the average temperature"
+    # Center exits the program. The screen won't fit the text to state this without decreasing font.
+    ev3.screen.print("RoomTemp", roomtemp, "°C")
+    ev3.screen.print("Left -][Right +")
     if Button.CENTER in ev3.buttons.pressed():
         break
-
-while True:
-    printtempies()
-    if tempies <= 10:
-        print("#2")
-        ev3.speaker.say("Cold")
-        ev3.screen.print("Cold!")
-        printtempies()
-    elif tempies >= 30:
-        print("#2")
-        ev3.speaker.say("Warm")
-        ev3.screen.print("Warm!")
-        printtempies()
+    elif Button.LEFT in ev3.buttons.pressed():
+        roomtemp = roomtemp - 1
+        wait(200)
+    elif Button.RIGHT in ev3.buttons.pressed():
+        roomtemp = roomtemp + 1
+        wait(200)
+    # elif Button.UP in ev3.buttons.pressed():
+    #     mewanttempies = 1
+    #     wait(200)
+    # elif Button.DOWN in ev3.buttons.pressed():
+    #     mewanttempies = 0
+    #     wait(200)
+    # elif mewanttempies == 1:
+    #     printtempies()
+    #     wait(200)
+    elif mewanttempies == 0:
+        eviltempies()
+        wait(200)
+evilfuckingtempies() # Runs to define evilfuckingtempies if not ran already
+# while True:
+#     printtempies()
+#     if tempies <= 10:
+#         print("#2")
+#         ev3.speaker.say("Cold!")
+#         ev3.screen.print("Cold!")
+#         printtempies()
+#     elif tempies >= 30:
+#         print("#2")
+#         ev3.speaker.say("Hands off.")
+#         ev3.screen.print("Warm!")
+#         printtempies()
