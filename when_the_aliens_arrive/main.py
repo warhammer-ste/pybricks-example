4e7f2eceb92a057eee6cb77b5e9d94de7be0fea9
AIK <92125439+warhammer-ste@users.noreply.github.com>
2025-12-17 08:07:10 -0600

    Update main.py
    Moved sacrificespeed above finishing beep, added a change in font so that it displays temperature MUCH easier
    Removed the speaking lines as it took too long to run those lines

--- when_the_aliens_arrive/main.py
+++ when_the_aliens_arrive/main.py
@@ -2,11 +2,12 @@
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
                                  InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.nxtdevices import (TemperatureSensor, SoundSensor, EnergyMeter, VernierAdapter) # This line is necessary for NXT devices
+from pybricks.nxtdevices import (TemperatureSensor)
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.media.ev3dev import Font
 
 ev3 = EV3Brick()
 left_thing_that_runs = Motor(Port.C)
@@ -14,6 +15,7 @@ right_motor = Motor(Port.B)
 sacrifice = Motor(Port.A) # Third evil motor that does usually nothing
 creek4win = DriveBase(left_thing_that_runs, right_motor, wheel_diameter=57.15, axle_track=88.9)
 theProbe = TemperatureSensor(Port.S1)
+LARGER_LARGER = Font(size=30)
 def evilasstempies():
     eviltempies = round(theProbe.temperature()) # Default value for referencing temp
     print("eviltempies", eviltempies)
@@ -26,25 +28,24 @@ def wiper():
     wait(200)
     ev3.screen.clear()
 
+sacrificespeed = 150
+ev3.screen.set_font(LARGER_LARGER)
 ev3.speaker.beep()
-sacrificespeed = 500
 
 while True:
-    eviltempies = round(theProbe.temperature())
     ev3.light.on(Color.GREEN)
+    eviltempies = round(theProbe.temperature())
     if eviltempies <= 15:
         ev3.light.on(Color.YELLOW)
         sacrifice.run(sacrificespeed)
         print(eviltempies, "Cold")
         ev3.screen.print("Cold!")
-        ev3.speaker.say("Cold")
         wiper()
     elif eviltempies >= 25:
         ev3.light.on(Color.RED)
         sacrifice.run(-sacrificespeed)
         print(eviltempies, "Warm")
         ev3.screen.print("Warm!")
-        ev3.speaker.say("Warm")
         wiper()
     else:
         sacrifice.stop()
